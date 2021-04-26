d1d31849dad7a830872e28616e697fb215a30916
Altan <admin@altan.me>
2021-04-26 14:51:17 +1200

    mvp

--- index.js
+++ index.js
@@ -10,14 +10,6 @@ const ping = require("ping");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-// Process APP_Secret for later Verification
-//app.use(xhub({ algorithm: "sha1", secret: app_secret }));
-//app.use(bodyParser.json());
-// app.use(express.json()); //Maybe can replace bodyParser with express
-
-// Parse URL-encoded bodies (as sent by HTML forms)
-// app.use(express.urlencoded());
-
 app.use(
   express.urlencoded({
     extended: true,
@@ -27,46 +19,38 @@ app.use(
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
+const cleanUrl = function (url) {
+  return url.replace(/(^\w+:|^)\/\//, "");
+};
+
 const pingHosts = async function (host) {
   let res = await ping.promise.probe(host, {
     timeout: 10,
-    extra: ["-i", "2"],
+    // extra: ["-i", "2"],
   });
-  console.log(res);
-  return res.alive;
+  return res;
 };
-
-// pingHosts(hosts);
 
 //Return IP on index page
 app.get("/", (req, res) => {
-  // let ip = req.headers["cf-connecting-ip"] || req.headers["x-forwarded-for"];
-  // let ip = req.socket.remoteAddress;
   res.render("index", { title: "Up Check" });
 });
 
 // Access the parse results as request.body
 app.post("/ping", async function (req, res) {
-  //   console.log(req.body.url);
-  let result = await pingHosts(req.body.url);
-  console.log(result);
-  res.json({ state: result });
-  //   res.send(pingHosts(req.body.url));
-  //   res.end();
+  console.log(`res = ${req.body.url}`);
+  console.log(`res = ${cleanUrl(req.body.url)}`);
+  let url = cleanUrl(req.body.url);
 
-  //...
-  //   res.end();
-  //   // Extract the validation errors from a request.
-  //   const errors = validationResult(req);
-  //   console.log(req);
-
-  //   if (!errors.isEmpty()) {
-  //     // There are errors. Render form again with sanitized values/errors messages.
-  //     // Error messages can be returned in an array using `errors.array()`.
-  //   } else {
-  //     // Data from form is valid.
-  //   }
-  //   console.log(request.body.user.url);
+  if (req.body.url == "") {
+    let error = "No URL / IP provided";
+    res.json({ error: error });
+  } else {
+    console.log("req has value");
+    let result = await pingHosts(url);
+    console.log(result);
+    res.json({ state: result.alive, ip: result.numeric_host, url: url });
+  }
 });
 
 // 404
---
+++ views/js/submit.js
@@ -0,0 +1,57 @@
+console.log("JS LOADED");
+const disp = document.getElementById("response");
+const detail = document.getElementById("detail");
+
+const populateUI = function (main, subcontent, visibility, height) {
+  document.getElementById("wrapper").style.height = height;
+  disp.innerText = main;
+
+  if (visibility) {
+    disp.style.display = "block";
+  } else {
+    disp.style.display = "none";
+  }
+  if (detail == "") {
+    detail.innerText = "";
+  } else {
+    detail.innerText = subcontent;
+    detail.style.display = "block";
+  }
+};
+
+const displayResponse = function (response) {
+  if (response.state == true) {
+    let innerText = `URL: ${response.url} IP: ${response.ip}`;
+    populateUI("Site looks up from here.", innerText, true, "160px");
+  } else if (response.state == false) {
+    populateUI("Site not responding.", " ", true, "127px");
+  } else {
+    populateUI(response.error, " ", true, "127px");
+  }
+};
+
+function logSubmit(event) {
+  event.preventDefault();
+  populateUI("Checking URL..", " ", true, "127px");
+  let url = document.getElementById("url").value;
+  const data = { url: url };
+  fetch("/ping", {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+    method: "POST",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+      displayResponse(data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+let form = document.getElementById("pingCheck");
+form.addEventListener("submit", logSubmit);
